98565f723a998352623a393453b22d5417909459
otacoo <208893667+otacoo@users.noreply.github.com>
2026-02-21 21:00:15 +0000

    don't check on start by default

--- Clover/app/src/main/java/org/otacoo/chan/core/settings/ChanSettings.java
+++ Clover/app/src/main/java/org/otacoo/chan/core/settings/ChanSettings.java
@@ -293,7 +293,7 @@ public class ChanSettings {
 
         updateCheckTime = new LongSetting(p, "update_check_time", 0L);
         updateCheckInterval = new LongSetting(p, "update_check_interval", UpdateManager.DEFAULT_UPDATE_CHECK_INTERVAL_MS);
-        autoCheckUpdates = new BooleanSetting(p, "preference_auto_check_updates", true);
+        autoCheckUpdates = new BooleanSetting(p, "preference_auto_check_updates", false);
 
         crashReporting = new BooleanSetting(p, "preference_crash_reporting", true);
         reencodeHintShown = new BooleanSetting(p, "preference_reencode_hint_already_shown", false);
