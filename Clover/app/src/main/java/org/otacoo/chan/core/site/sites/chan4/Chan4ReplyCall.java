fe5492214e82f22ffdada8bad5a50beff5d5f389
otacoo <208893667+otacoo@users.noreply.github.com>
2026-02-22 20:09:46 +0000

    Update Chan4ReplyCall.java

--- Clover/app/src/main/java/org/otacoo/chan/core/site/sites/chan4/Chan4ReplyCall.java
+++ Clover/app/src/main/java/org/otacoo/chan/core/site/sites/chan4/Chan4ReplyCall.java
@@ -58,7 +58,6 @@ public class Chan4ReplyCall extends CommonReplyHttpCall {
         requestBuilder.header("Accept",
                 "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
         requestBuilder.header("Accept-Language", "en-US,en;q=0.5");
-        requestBuilder.header("Accept-Encoding", "gzip");
         requestBuilder.header("Connection", "keep-alive");
         requestBuilder.header("Sec-Fetch-Dest", "document");
         requestBuilder.header("Sec-Fetch-Mode", "navigate");
