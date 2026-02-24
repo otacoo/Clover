b598246ab0d9c1522b6fb0bb50221cc4bc65d17f
otacoo <208893667+otacoo@users.noreply.github.com>
2026-02-24 22:20:20 +0000

    Update Chan.java

--- Clover/app/src/main/java/org/otacoo/chan/Chan.java
+++ Clover/app/src/main/java/org/otacoo/chan/Chan.java
@@ -25,6 +25,8 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.webkit.WebSettings;
 
+import androidx.annotation.NonNull;
+
 import org.codejargon.feather.Feather;
 import org.otacoo.chan.core.database.DatabaseManager;
 import org.otacoo.chan.core.di.AppModule;
@@ -204,34 +206,34 @@ public class Chan extends Application implements
     }
 
     @Override
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+    public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
         activities.add(activity);
     }
 
     @Override
-    public void onActivityStarted(Activity activity) {
+    public void onActivityStarted(@NonNull Activity activity) {
         activityEnteredForeground();
     }
 
     @Override
-    public void onActivityResumed(Activity activity) {
+    public void onActivityResumed(@NonNull Activity activity) {
     }
 
     @Override
-    public void onActivityPaused(Activity activity) {
+    public void onActivityPaused(@NonNull Activity activity) {
     }
 
     @Override
-    public void onActivityStopped(Activity activity) {
+    public void onActivityStopped(@NonNull Activity activity) {
         activityEnteredBackground();
     }
 
     @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+    public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
     }
 
     @Override
-    public void onActivityDestroyed(Activity activity) {
+    public void onActivityDestroyed(@NonNull Activity activity) {
         activities.remove(activity);
     }
 }
